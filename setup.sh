767c0ef9131a98d1c3e0fbad7249dba95c0a0200
Mun Kit "Jeff" Tham <jeff.tham@email.com>
2017-07-17 08:57:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt-get upgrade -y
 # download nodejs setup script
 pushd ~
 
-curl -sL https://deb.nodesource.com/setup_7.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo apt-get install build-essential -y
